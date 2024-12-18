c22faabd31830cb3db12c063109d38c8efb532db
Noeyhn <hibnb@naver.com>
2024-12-18 22:05:03 +0900

    test2

--- CupangClone/build/generated/sources/annotationProcessor/java/main/com/github/cupangclone/service/mapper/UserPrincipalMapperImpl.java
+++ CupangClone/build/generated/sources/annotationProcessor/java/main/com/github/cupangclone/service/mapper/UserPrincipalMapperImpl.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-17T19:02:51+0900",
+    date = "2024-12-18T22:04:39+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.11.1.jar, environment: Java 17 (Oracle Corporation)"
 )
 public class UserPrincipalMapperImpl implements UserPrincipalMapper {
